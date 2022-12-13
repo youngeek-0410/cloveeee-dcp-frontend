f0bad447e91c25f171421bdd792b061914df8455
igsr5 <igsr5.dev@gmail.com>
2022-12-14 03:47:19 +0900

    react-typed

--- src/common/FirstLoadView/FirstLoadView.tsx
+++ src/common/FirstLoadView/FirstLoadView.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Typed from "react-typed";
+
+import { styled } from "../../stitches.config";
 
 type Props = {
   receiverName: string;
 };
 export const FirstLoadView: React.FC<Props> = ({ receiverName }) => {
-  return <div>{receiverName}</div>;
+  return (
+    <Base>
+      <Typed strings={[`${receiverName}さんへ`]} typeSpeed={110} startDelay={100} />
+      <p>Presents by Cloveeee</p>
+    </Base>
+  );
 };
+
+const Base = styled("div", {
+  height: "100svh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
