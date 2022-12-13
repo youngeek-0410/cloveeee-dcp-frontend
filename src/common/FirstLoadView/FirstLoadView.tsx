d03f4d366643e9c5871b14457858daf45ce2d719
igsr5 <igsr5.dev@gmail.com>
2022-12-14 04:15:30 +0900

    Implement FirstLoadView

--- src/common/FirstLoadView/FirstLoadView.tsx
+++ src/common/FirstLoadView/FirstLoadView.tsx
@@ -1,3 +1,4 @@
+import { motion, useAnimationControls } from "framer-motion";
 import React from "react";
 import Typed from "react-typed";
 
@@ -5,20 +6,54 @@ import { styled } from "../../stitches.config";
 
 type Props = {
   receiverName: string;
+  onLoadComplete?: () => void;
 };
-export const FirstLoadView: React.FC<Props> = ({ receiverName }) => {
+
+export const FirstLoadView: React.FC<Props> = ({ receiverName, onLoadComplete }) => {
+  const pControls = useAnimationControls();
+  const baseControls = useAnimationControls();
+  const onTypeComplete = async () => {
+    await pControls.start({
+      opacity: 1,
+    });
+    await baseControls.start({
+      opacity: 0,
+    });
+    onLoadComplete?.();
+  };
+
   return (
-    <Base>
-      <Typed strings={[`${receiverName}さんへ`]} typeSpeed={110} startDelay={100} />
-      <p>Presents by Cloveeee</p>
+    <Base
+      animate={baseControls}
+      transition={{
+        duration: 1.5,
+        delay: 1,
+      }}
+    >
+      <Typed strings={[`${receiverName}さんへ`]} typeSpeed={150} startDelay={100} onComplete={onTypeComplete} />
+      <motion.p
+        style={{ opacity: 0 }}
+        animate={pControls}
+        transition={{
+          duration: 1.3,
+          delay: 0.5,
+        }}
+      >
+        Presents by Cloveeee.
+      </motion.p>
     </Base>
   );
 };
 
-const Base = styled("div", {
-  height: "100svh",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-});
+const Base = motion(
+  styled("div", {
+    height: "100svh",
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    letterSpacing: "0.1em",
+  })
+);
