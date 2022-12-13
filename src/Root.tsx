f74974aa176c3b18299dbf4e6173d0c0bfb29099
igsr5 <igsr5.dev@gmail.com>
2022-12-14 05:40:59 +0900

    Implement switch first load view

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,60 @@
-import React from "react";
+import { motion, useAnimationControls } from "framer-motion";
+import React, { useEffect, useState } from "react";
 
+import { FirstLoadView } from "./common/FirstLoadView/FirstLoadView";
 import { Project } from "./domain/type";
 import { HeroPart } from "./HeroPart/HeroPart";
+import { styled } from "./stitches.config";
 
 type Props = {
   project: Project;
 };
 
 const RootContainer: React.FC<Props> = ({ project }) => {
-  return <Root project={project} />;
+  const [isFirstLoadCompoleted, setIsFirstLoadCompleted] = useState(false);
+  const mainPartControls = useAnimationControls();
+  const onFirstLoadComplete = () => {
+    setIsFirstLoadCompleted(true);
+  };
+
+  useEffect(() => {
+    // NOTE: render直後に実行したい
+    mainPartControls.start({
+      opacity: 1,
+    });
+  }, [isFirstLoadCompoleted, mainPartControls]);
+
+  return (
+    <>
+      {!isFirstLoadCompoleted ? (
+        <FirstLoadPart>
+          <FirstLoadView receiverName={project.receiver_name} onLoadComplete={onFirstLoadComplete} />
+        </FirstLoadPart>
+      ) : (
+        <MainPart
+          animate={mainPartControls}
+          style={{ opacity: 0, overflow: "hidden" }}
+          transition={{
+            duration: 1,
+            delay: 0.2,
+          }}
+        >
+          <Root project={project} />
+        </MainPart>
+      )}
+    </>
+  );
 };
+
+const FirstLoadPart = styled("div", {
+  position: "fixed",
+  top: 0,
+  left: 0,
+
+  width: "100vw",
+  height: "100vh",
+});
+const MainPart = motion(styled("div", {}));
 
 export default RootContainer;
 
