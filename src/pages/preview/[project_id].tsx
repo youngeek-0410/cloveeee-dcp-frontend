8746f0c7251d2d4edf203bdec398d13ff82eddc8
igsr5 <igsr5.dev@gmail.com>
2022-12-14 03:13:47 +0900

    Add page `/preview/[project_id]`

---
+++ src/pages/preview/[project_id].tsx
@@ -0,0 +1,61 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { GetServerSideProps, NextPage } from "next";
+
+import { backendApiUrl } from "../../apiUrl";
+import { Project } from "../../domain/type";
+import { GeneralPageProps } from "../_app";
+
+type Props = {
+  project: Project;
+} & GeneralPageProps;
+
+const Page: NextPage<Props> = ({ project }) => {
+  return <div>{project.receiver_name}</div>;
+};
+
+export default Page;
+
+type GetProjectApiRequest = {
+  text_message_limit: number;
+  image_message_limit: number;
+};
+
+type GetProjectApiResponse = Project;
+
+/*
+ * デジタル色紙のプレビューページ
+ * プレビュー色紙はProjectデータが動的に変わるなのでSSRで配信する
+ */
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const { project_id } = ctx.query;
+
+  const params: GetProjectApiRequest = {
+    text_message_limit: 20, // NOTE: まだ件数が多い時の仕様が固まってないので適当な値
+    image_message_limit: 20, // NOTE: まだ件数が多い時の仕様が固まってないので適当な値
+  };
+
+  const requestConfig: AxiosRequestConfig<GetProjectApiRequest> = {
+    url: `${backendApiUrl}/api/projects/${project_id}/`,
+    method: "GET",
+    params,
+    headers: {
+      Accept: "application/json",
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, status } = await axios.request<GetProjectApiResponse>(requestConfig);
+  if (status === 404) {
+    return {
+      notFound: true,
+    };
+  } else if (status !== 200) {
+    throw new Error("Failed to get project");
+  }
+
+  return {
+    props: {
+      project: data,
+    },
+  };
+};
