b44cb28c97ce7c47578671c8b406423c0191ebee
igsr5 <igsr5.dev@gmail.com>
2022-12-15 10:39:53 +0900

    Add Music.tsx

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Header } from "./Header";
 import { LeadText } from "./LeadText";
+import { Music } from "./Music";
 
 export const MainContent: React.FC = () => {
   return (
     <>
       <Header />
       <LeadText />
+      <Music />
     </>
   );
 };
--- src/Music.tsx
+++ src/Music.tsx
@@ -2,21 +2,16 @@ import React from "react";
 
 import { styled } from "./stitches.config";
 
-export const LeadText: React.FC = () => {
+export const Music: React.FC = () => {
   return (
     <Base>
       <h2>
-        あなただけの
+        思い出の
         <br />
-        世界でたった1つだけの
-        <br />
-        Webサイト
+        音楽
       </h2>
-      <p>
-        このサイトはあなたへの感謝の言葉や思い出がたくさん詰まった世界でたった一つしかないのあなただけのWebサイトです
-      </p>
 
-      {/* TODO: ユーザ投稿の写真を入れる？ */}
+      <p>再生ボタンを押すと音楽を聴くことができます</p>
       <img src="https://via.placeholder.com/350x200" alt="" />
     </Base>
   );
@@ -25,15 +20,16 @@ export const LeadText: React.FC = () => {
 const Base = styled("div", {
   width: "95%",
   margin: "0 auto",
-  padding: "0 0 60px",
+  padding: "60px 0",
 
   "& h2": {
-    fontSize: "2.5rem",
+    fontSize: "2rem",
     fontWeight: "300",
     letterSpacing: "0.1rem",
     lineHeight: "1.5",
+    display: "inline",
+    background: "linear-gradient(transparent 60%, #ffdcb8 0%)",
   },
-
   "& img": {
     display: "block",
     width: "100%",
