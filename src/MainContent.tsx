7dfa0d08d096c95d8d45fc81a7fd097c8f20325d
igsr5 <igsr5.dev@gmail.com>
2022-12-15 14:37:52 +0900

    Fix

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -4,8 +4,8 @@ import { Header } from "./common/Header";
 import { LeadText } from "./leadText/LeadText";
 import { MemorialMusic } from "./memorialMusic/MemorialMusic";
 import { MemorialPhoto } from "./memorialPhoto/MemorialPhoto";
-import { TextMessage } from "./textMessage/textMessage";
 import { Footer } from "./common/Footer";
+import { TextMessage } from "./textMessage/TextMessage";
 
 export const MainContent: React.FC = () => {
   return (
