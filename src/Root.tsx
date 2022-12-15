f4265630e9e359ece5e3a08d53cfc0ad262e3644
igsr5 <igsr5.dev@gmail.com>
2022-12-15 11:14:09 +0900

    Revert "Remove Root.tsx"
    This reverts commit afcb045ff881de0b05fa32219a6a76398e5904ed.

---
+++ src/Root.tsx
@@ -0,0 +1,56 @@
+import { motion, useAnimationControls } from "framer-motion";
+import React, { useEffect, useState } from "react";
+
+import { FirstLoadView } from "./firstLoadView/FirstLoadView";
+import { Project } from "./domain/type";
+import { MainContent } from "./MainContent";
+import { styled } from "./stitches.config";
+
+type Props = {
+  project: Project;
+};
+
+export const RootContainer: React.FC<Props> = ({ project }) => {
+  const [isFirstLoadCompoleted, setIsFirstLoadCompleted] = useState(false);
+  const mainPartControls = useAnimationControls();
+  const onFirstLoadComplete = () => {
+    setIsFirstLoadCompleted(true);
+  };
+
+  useEffect(() => {
+    // NOTE: render直後に実行したい
+    mainPartControls.start({
+      opacity: 1,
+    });
+  }, [isFirstLoadCompoleted, mainPartControls]);
+
+  return (
+    <>
+      {!isFirstLoadCompoleted ? (
+        <FirstLoadPart>
+          <FirstLoadView receiverName={project.receiver_name} onLoadComplete={onFirstLoadComplete} />
+        </FirstLoadPart>
+      ) : (
+        <motion.div
+          animate={mainPartControls}
+          style={{ opacity: 0, overflow: "hidden" }}
+          transition={{
+            duration: 1,
+            delay: 0.2,
+          }}
+        >
+          <MainContent />
+        </motion.div>
+      )}
+    </>
+  );
+};
+
+const FirstLoadPart = styled("div", {
+  position: "fixed",
+  top: 0,
+  left: 0,
+
+  width: "100vw",
+  height: "100vh",
+});
